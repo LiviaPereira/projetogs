d747c4bf4db279ffd501a7e1accecf8200e16cb6
Chariskyrim <enzolucianox@gmail.com>
2025-06-06 20:39:15 -0300

    Alterações do logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
   return (
     <nav className="bg-red-600 text-white p-4 flex justify-between items-center font-montserrat">
       {/* Apply w-X and h-X classes to the img tag */}
-      <img src={alertaBranco} alt="Logo do projeto" className="h-20 w-auto" /> {/* Example: h-10 will set height to 40px */}
+      <img src={alertaBranco} alt="Logo do projeto" className="h-20 w-auto ml-10" /> {/* Example: h-10 will set height to 40px */}
       <div className="space-x-4">
         <a href="/" className="hover:underline transition duration-300">Home</a>
         <a href="/sobre" className="hover:underline transition duration-300">Sobre</a>
